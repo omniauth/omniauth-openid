225d9509a5e217e7e9a2f1223723e211e3ec70b7
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 22:18:05 -0600

    Restore omniauth-openid version module API

--- lib/omniauth-openid.rb
+++ lib/omniauth-openid.rb
@@ -3,13 +3,12 @@ require "version_gem"
 require "rexml"
 
 # this library's version
-
-# Configure version before loading the rest of the library
-
-# This library
-require "omniauth/strategies/open_id"
 require_relative "omniauth/openid/version"
 
+# Configure version before loading the rest of the library
 OmniAuth::OpenID::Version.class_eval do
   extend VersionGem::Basic
 end
+
+# This library
+require "omniauth/strategies/open_id"
